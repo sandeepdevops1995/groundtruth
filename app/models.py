f6ec4bff35e56ac5fb128db2930da34f2ad51f0e
Varshitha Rani <varshitharani.gaggalapalli@soctronics.com>
2023-03-27 19:37:48 +0530

    added seal_no for rake

--- app/models.py
+++ app/models.py
@@ -892,5 +892,6 @@ class Diagnostics(db.Model):
     url = db.Column(db.String(200))
     request = db.Column(db.JSON)
     response =  db.Column(db.JSON)
+    created_at = db.Column(db.DateTime(), default = datetime.now)
     
     __tablename__ = 'Diagnostics'

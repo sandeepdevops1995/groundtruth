c85d092814e4d613ee0ed92b98cb2054f08d8b84
gnagalakshmi <lakshmi@atai.ai>
2023-04-19 15:42:13 +0530

    changed creation of env file from specific targets to make target install

--- exportenvs.sh
+++ exportenvs.sh
@@ -21,14 +21,10 @@ COMPOSE_IS_MOCK_ENABLED=${IS_MOCK_ENABLED}
 COMPOSE_IS_EVENT_BASED=${IS_EVENT_BASED}
 COMPOSE_CCLS_GROUND_TRUTH=${CCLS_GROUND_TRUTH}
 
-#remove .env file
-rm -f ./.env
-
 #read .env file in env folder
 env_file_path="./env/.env"
 
 _env_init(){
-
 if ( [ $COMPOSE_IP_ADDRESS ] ) then 
     echo "ip address",${IP_ADDRESS}
 else 

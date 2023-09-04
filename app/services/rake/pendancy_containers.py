d9ef2d74b88bc57b223cf10d305062ae84b85435
gvarshitharani <varshitharani@atai.ai>
2023-09-04 19:07:51 +0530

    changed output format for pendancy container

--- app/services/rake/pendancy_containers.py
+++ app/services/rake/pendancy_containers.py
@@ -119,7 +119,7 @@ class PendancyService():
             if config.GROUND_TRUTH == GroundTruthType.ORACLE.value:
                 pass
             elif config.GROUND_TRUTH == GroundTruthType.SOAP.value:
-                final_data = []
+                final_data = {}
                 for pendency_type,gateway_ports in pendency_types:
                     pendency_type = int(pendency_type)
                     if PendencyType.LOADED.value == pendency_type:
@@ -130,7 +130,7 @@ class PendancyService():
                                             'P_CUTOFF_DATE': ''}
                             data = soap_service.get_pendancy_details(request_params)
                             if data:
-                                final_data += data
+                                final_data[PendencyType.LOADED.value] =  PendancyService.format_data_from_ccls(data,True)
                     elif PendencyType.EMPTY.value == pendency_type:
                         logger.info("fetching EMPTY pendancy containers"+str(gateway_ports))
                         for port in gateway_ports:
@@ -146,7 +146,7 @@ class PendancyService():
                                     obj['sealNo'] = 'N/A'
                                     obj['odcFlg'] = 'N/A'
                                     obj['wt'] = 0
-                                final_data += data
+                                final_data[PendencyType.EMPTY.value] = PendancyService.format_data_from_ccls(data,True)
 
                     elif PendencyType.BLOCK.value == pendency_type:
                         logger.info("fetching BLOCK pendancy containers"+str(gateway_ports))
@@ -154,7 +154,7 @@ class PendancyService():
                             request_params = {'GW_PORT_CODE': port}
                             data = soap_service.get_block_pendancy_details(request_params)
                             if data:
-                                final_data += data
+                                final_data[PendencyType.BLOCK.value] = PendancyService.format_data_from_ccls(data,True)
 
                     elif PendencyType.EXPRESS.value == pendency_type:
                         logger.info("fetching EXPRESS pendancy containers"+str(gateway_ports))
@@ -162,7 +162,7 @@ class PendancyService():
                             request_params = {'GW_PORT_CODE': port}
                             data = soap_service.get_express_pendancy_details(request_params)
                             if data:
-                                final_data += data
+                                final_data[PendencyType.EXPRESS.value] = PendancyService.format_data_from_ccls(data,True)
 
                     elif PendencyType.LCL.value == pendency_type:
                         logger.info("fetching LCL pendancy containers"+str(gateway_ports))
@@ -172,16 +172,17 @@ class PendancyService():
                         #     request_params = {'GW_PORT_CODE': port}
                         #     data = soap_service.get_lcl_pendancy_details(request_params)
                         #     if data:
-                        #         final_data += data
+                        #         final_data[PendencyType.LCL.value] = PendancyService.format_data_from_ccls(data,True)
 
                 if final_data:
-                    data = PendancyService.format_data_from_ccls(final_data,True)
+                    # data = PendancyService.format_data_from_ccls(final_data,True)
                     #logger.info("data fetched from CCLS service: "+str(data))
-                    return json.dumps(data)
+                    return json.dumps(final_data)
                 
             data = PendancyContainer.query.filter(PendancyContainer.gateway_port_code.in_(gateway_ports)).order_by(PendancyContainer.gateway_port_code).all()
             logger.info("data fetched from local db")
-            return db_functions(data).as_json()
+            data = db_functions(data).as_json()
+            return {0:data}
         except Exception as e:
             logger.exception(str(e))
             if isRetry and count >= 0 :
